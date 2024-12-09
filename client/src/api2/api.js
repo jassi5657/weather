8b0b179fc64c9049c99a8537c6733eae8b286333
jassi5657 <preetsansoya008@gmail.com>
2024-12-09 14:10:18 +0530

    env added

--- client/src/api2/api.js
+++ client/src/api2/api.js
@@ -1,5 +1,4 @@
-const apiKey = 'c68e1e5aa95613f97f24f8aa4fe0b522';
-
+const apiKey = import.meta.env.VITE_API_KEY;
 const getWeather = async (city) => {
     return await fetch(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`)
     .then((res) => res.json())
